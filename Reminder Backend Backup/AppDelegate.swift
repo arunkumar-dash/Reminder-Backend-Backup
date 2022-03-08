4d57d16aa9db2bdb2bdaf515acbc96814c6cb9dd
arunkumar-dash <arun.kumar.dash77@gmail.com>
2022-03-08 11:31:46 +0530

    Initial Commit

---
+++ Reminder Backend Backup/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Reminder Backend Backup
+//
+//  Created by Arun Kumar on 08/03/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
